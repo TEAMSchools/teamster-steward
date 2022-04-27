7abf40124afbf7217ad0652ecdd5e2b258810c67
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-04-27 15:36:06 -0500

    remove limitrange

--- .helm/helm-install.sh
+++ .helm/helm-install.sh
@@ -3,4 +3,7 @@ helm repo update
 
 envsubst < ./.helm/values-override.yaml.tmpl > ./.helm/values-override.yaml
 
-helm upgrade --install user-cloud dagster-cloud/dagster-cloud-agent --namespace dagster-cloud -f ./.helm/values-override.yaml
+helm upgrade --install user-cloud dagster-cloud/dagster-cloud-agent \
+    --namespace dagster-cloud \
+    -f ./.helm/values-override.yaml
+    
