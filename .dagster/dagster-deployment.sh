7552dc7293eeb2cc08b3d74b3a2ac214dc7f0ff1
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-05-16 13:22:41 -0500

    update deps, automate deployment

---
+++ .dagster/dagster-deployment.sh
@@ -0,0 +1,4 @@
+dagster-cloud deployment settings set-from-file ./.dagster/deployment-settings.yaml
+
+envsubst < ./.dagster/alerts.yaml.tmpl > ./.dagster/alerts.yaml
+dagster-cloud deployment alert-policies sync -a ./.dagster/alerts.yaml
--- .google/k8s-init.sh
+++ .google/k8s-init.sh
@@ -1,5 +1,8 @@
 kubectl create namespace dagster-cloud
 kubectl create secret generic dagster-cloud-agent-token \
+    --save-config \
+    --dry-run=client \
     --from-literal=DAGSTER_CLOUD_AGENT_TOKEN=${DAGSTER_CLOUD_AGENT_TOKEN} \
-    --namespace dagster-cloud
-    
+    --namespace dagster-cloud \
+    -o yaml \
+    | kubectl apply -f -
